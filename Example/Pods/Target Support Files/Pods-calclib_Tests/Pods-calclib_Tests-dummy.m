ed5bc85b8e6e3199b3a7654783b66c67e72a8487
krishsajeev <88449518+krishsajeev@users.noreply.github.com>
2023-01-26 14:00:35 +0530

    Add files via upload

---
+++ Example/Pods/Target Support Files/Pods-calclib_Tests/Pods-calclib_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_calclib_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_calclib_Tests
+@end
---
+++ Example/Pods/Target Support Files/calclib/calclib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_calclib : NSObject
+@end
+@implementation PodsDummy_calclib
+@end
