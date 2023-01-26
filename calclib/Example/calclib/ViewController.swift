d08564d52308842533f01cd211b85a62f66ddf14
krishsajeev <88449518+krishsajeev@users.noreply.github.com>
2023-01-26 13:37:57 +0530

    Add files via upload

---
+++ calclib/Example/calclib/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  calclib
+//
+//  Created by sajeev.rk on 01/26/2023.
+//  Copyright (c) 2023 sajeev.rk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
