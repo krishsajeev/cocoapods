d0cc2fa4dd4d502f1e6dcd569ab644762d3a8cac
krishsajeev <88449518+krishsajeev@users.noreply.github.com>
2023-01-26 13:59:20 +0530

    Delete calclib directory

--- calclib/Example/Pods/Target Support Files/Pods-calclib_Tests/Pods-calclib_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_calclib_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_calclib_Tests
-@end
--- calclib/Example/Pods/Target Support Files/calclib/calclib-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_calclib : NSObject
-@end
-@implementation PodsDummy_calclib
-@end
